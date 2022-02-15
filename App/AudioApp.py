77b67fcaf2e699c2964899712507a9e5567eda30
ChadG-202 <chadgarratt202@gmail.com>
2022-02-15 18:25:22 +0000

    Audio App Dev

--- App/AudioApp.py
+++ App/AudioApp.py
@@ -1,15 +1,76 @@
 from tkinter import *
-from tkinter import messagebox
+from mysqlx import Statement
+import sounddevice as sound
+from scipy.io.wavfile import write
+import time
 
-root=Tk()
-root.geometry("600x700+400+80")
-root.resizable(False, False)
-root.title("Voice Recorder")
-root.configure(background="#4a4a4a")
+class RecorderApp:
+    def __init__(self, window, pos=0, emotion="Happy"):
+        self.pos = pos
+        self.emotion = emotion
+        self.root = window
+        self.root.geometry("600x500")
+        self.root.resizable(False, False)
+        self.root.title("Voice Recorder")
+        self.root.configure(background="#4a4a4a")
 
-# Logo
-photo = "rec-button.png"
-myimage=Label(image=photo,background="#4a4a4a")
-myimage.pack(padx=5,pady=5)
+        #Icon
+        self.image_icon=PhotoImage(file="App/rec-button.png")
+        self.root.iconphoto(False, self.image_icon)
 
-root.mainloop()
+        #Logo
+        self.photo=PhotoImage(file="App/rec-button.png")
+        self.myimage=Label(image=self.photo,background="#4a4a4a")
+        self.myimage.pack(pady=30)
+
+        #Name
+        Label(text="Voice Recorder", font="arial 30 bold", background="#4a4a4a", fg="white").pack()
+
+        #Button
+        self.record = Button(self.root, font="arial 20", text="Record",bg="#111111",fg="white",border=0,command=self.Record).pack(pady=30)
+
+        self.sentence()
+
+        self.root.mainloop()
+
+    def Recording(self, type, path):
+        freq=22050
+        dur=4
+        channel=2
+        recording=sound.rec(dur*freq, samplerate=freq,channels=channel)
+
+        #Timer
+        timeLeft=dur
+        while timeLeft>0:
+            timeLeft-=1
+            Label(text=f"{str(timeLeft)}", font="arial 40",width=4,background="#4a4a4a").place(x=240, y=420)
+            self.root.update()
+            time.sleep(1)
+
+        sound.wait()
+        write("App/AppData/Audio/"+type+"/"+path+".wav",freq,recording)
+
+        self.pos +=1
+        self.sentence()
+        if self.pos == 30:
+            quit()
+
+    def Record(self):
+        self.Recording(self.emotion, str(self.pos))
+    
+    def sentence(self):
+        questions = ["'Can you help me?'", "'Who are you?'",
+        "'Where am I?'", "'Why is this happening?'", "'What time is it?'", 
+        "'Take me home?'", "'Whats the weather today?'", "'Set a timer for 10 minutes?'",
+        "'How do you spell tree?'", "'Whats 10 + 20?'"]
+
+        tempPos = self.pos
+        if self.pos > 9 and self.pos < 20:
+            tempPos = self.pos - 10
+            self.emotion = "Neutral"
+        elif self.pos > 19 and self.pos < 30:
+            tempPos = self.pos - 20
+            self.emotion = "Sad"
+
+        ask = "Say the phrase: "+questions[tempPos]+" in a "+self.emotion+" voice."
+        Label(text=f"{ask}", font="arial 15",width=50,background="#4a4a4a",fg="white").place(x=20, y=350)
--- App/main.py
+++ App/main.py
@@ -1,5 +1,7 @@
 from PhotoApp import ImageApp
+from AudioApp import RecorderApp
 import tkinter as tk
 
 if __name__ == "__main__":
     ImageApp(tk.Tk(),'Take Happy Photo')
+    RecorderApp(tk.Tk())
