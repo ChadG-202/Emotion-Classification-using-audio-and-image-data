576f47b2c8c6237f7b07cdf0df1ef80804fef2d7
ChadG-202 <chadgarratt202@gmail.com>
2022-03-01 15:57:51 +0000

    fix

--- Windows/Start_window.py
+++ Windows/Start_window.py
@@ -16,24 +16,25 @@ class Start():
         tk.Label(text="Emotion Chatbot", font="arial 30 bold", background="#4a4a4a", fg="white").pack()
 
         #Button
-        self.next = tk.Button(self.root, font="arial 20", text="Next",bg="#C1E1C1",fg="black",border=0,command=self.next).pack(pady=30)
+        self.nextB = tk.Button(self.root, font="arial 20", text="Next",bg="#C1E1C1",fg="black",border=0,command=self.next).pack(pady=30)
 
         self.sentence()
         self.root.mainloop()
 
-        def next(self):
-            self.pos += 1
+    def next(self):
+        self.pos += 1
+        self.sentence()
 
-        def sentence(self):
-            text = ""
-            if self.pos == 0:
-                text = "page 1"
-            elif self.pos == 1:
-                text = "page 2"
-            elif self.pos == 2:
-                text = "page 3"
-            else:
-                self.root.destroy()
+    def sentence(self):
+        text = ""
+        if self.pos == 0:
+            text = "page 1"
+        elif self.pos == 1:
+            text = "page 2"
+        elif self.pos == 2:
+            text = "page 3"
+        else:
+            self.root.destroy()
 
-            if self.pos < 3:
-                tk.Label(text=f"{text}", font="arial 15",width=50,background="#4a4a4a",fg="white").place(x=45, y=450)
+        if self.pos < 3:
+            tk.Label(text=f"{text}", font="arial 15",width=50,background="#4a4a4a",fg="white").place(x=45, y=450)
